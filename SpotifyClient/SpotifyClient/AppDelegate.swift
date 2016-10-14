bb8bc6ddab1f3ee962bd828a27c7df9ce6faeef2
Daniel Inoa <danielinoa@gmail.com>
2016-10-14 15:14:11 -0400

    Sets up project

---
+++ SpotifyClient/SpotifyClient/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SpotifyClient
+//
+//  Created by Daniel Inoa on 10/14/16.
+//  Copyright © 2016 Daniel Inoa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ SpotifyClient/SpotifyClient/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpotifyClient
+//
+//  Created by Daniel Inoa on 10/14/16.
+//  Copyright © 2016 Daniel Inoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
